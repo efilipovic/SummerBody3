297b0b2f764ff1824fc482152e2e822a5ae35186
Ena Filipović (Span) <efilipovic@span.eu>
2020-06-14 23:37:22 +0200

    Added workouts changing onclick, gifs wrapped in card view

--- app/src/main/java/enafilipovic/ferit/summerbody/Activities/ProgramWorkoutActivity.kt
+++ app/src/main/java/enafilipovic/ferit/summerbody/Activities/ProgramWorkoutActivity.kt
@@ -3,6 +3,7 @@ package enafilipovic.ferit.summerbody.Activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import enafilipovic.ferit.summerbody.R
 import enafilipovic.ferit.summerbody.Repositories.WorkoutRepo
 import kotlinx.android.synthetic.main.activity_program_details.*
@@ -15,30 +16,47 @@ class ProgramWorkoutActivity : AppCompatActivity() {
         const val KEY_PROGRAM_ID: String = "KEY_ID"
     }
 
+
+    var workout_id: Int =0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_program_exercises)
+
+
+        var program_id = intent.getIntExtra(KEY_PROGRAM_ID,0)
 
         back_arrow.setOnClickListener{
             startActivity(Intent(this, ProgramDetailsActivity::class.java))
             finish()
         }
 
-     //   button_startworkout.setOnClickListener{
-            //updateUI()
-   //     }
+     button_nextworkout.setOnClickListener{
+        setUpUI(program_id,workout_id)
+     }
 
-        setUpUI(intent.getIntExtra(KEY_PROGRAM_ID,0))
+        setUpUI(program_id,workout_id)
     }
 
-    private fun setUpUI(id: Int){
+   // private fun setUpUI(id: Int){
        // val workout_id=intent.getIntExtra(KEY_ID,0)
-        title_program_workout.text=WorkoutRepo.workouts[id-1].workouts[0].workout_title
-        workout_item_image.setImageResource(WorkoutRepo.workouts[id-1].workouts[0].workout_image)
-        workout_item_description.text=WorkoutRepo.workouts[id-1].workouts[0].workout_description
-    }
+    //      title_program_workout.text=WorkoutRepo.workouts[id-1].workouts[0].workout_title
+     //   workout_item_image.setImageResource(WorkoutRepo.workouts[id-1].workouts[0].workout_image)
+      //  workout_item_description.text=WorkoutRepo.workouts[id-1].workouts[0].workout_description
+     //   workout_id++
+   // }
 
-    private fun updateUI(id: Int, workout_id:Int){
-      //TODO
+    private fun setUpUI(id: Int, w_id:Int){
+        if(w_id == WorkoutRepo.workouts[id-1].workouts.size){
+            Toast.makeText(baseContext, "Congrats, program finished!.",
+                Toast.LENGTH_SHORT
+            ).show()
+            finish()
+        }else {
+            title_program_workout.text = WorkoutRepo.workouts[id - 1].workouts[w_id].workout_title
+            workout_item_image.setImageResource(WorkoutRepo.workouts[id - 1].workouts[w_id].workout_image)
+            workout_item_description.text =
+                WorkoutRepo.workouts[id - 1].workouts[w_id].workout_description
+            workout_id++
+        }
     }
 }
--- app/src/main/java/enafilipovic/ferit/summerbody/Repositories/WorkoutRepo.kt
+++ app/src/main/java/enafilipovic/ferit/summerbody/Repositories/WorkoutRepo.kt
@@ -20,112 +20,119 @@ object WorkoutRepo {
                                 "2. Lean forward and place your hands flat on the wall, in line with your shoulders. Your body should be in plank position, with your spine straight, not sagging or arched.\n" +
                                 "3. Lower your body toward the wall and then push back.\n" +
                                 "4. Repeat 10 times.",
-                        "Wall Pushups"
+                        "Wall pushups"
                     ),
                     ProgramWorkoutItem(
                         R.drawable.pelvic_tilts,
-                        "1. Stand about 3 feet away from a wall, facing the wall, with your feet shoulder-width apart.\n" +
-                                "2. Lean forward and place your hands flat on the wall, in line with your shoulders. Your body should be in plank position, with your spine straight, not sagging or arched.\n" +
-                                "3. Lower your body toward the wall and then push back.\n" +
-                                "4. Repeat 10 times.",
-                        "Pelvic Tilts"
+                        "1. Take a deep breath, tighten your buttocks, and tilt your hips slightly forward.\n" +
+                                "2.Hold for a 3-count.\n" +
+                                "3. Now tilt your hips back, and hold for 3 seconds. (It’s a very subtle movement.)\n" +
+                                "4. Repeat 8 to 12 times.",
+                        "Pelvic tilts"
                     ),
                     ProgramWorkoutItem(
                         R.drawable.shblade_squeeze,
-                        "1. Stand about 3 feet away from a wall, facing the wall, with your feet shoulder-width apart.\n" +
-                                "2. Lean forward and place your hands flat on the wall, in line with your shoulders. Your body should be in plank position, with your spine straight, not sagging or arched.\n" +
-                                "3. Lower your body toward the wall and then push back.\n" +
-                                "4. Repeat 10 times.",
-                        "Shoulderblade Squeeze"
+                        "1. Sit up straight in your seat, rest your hands in your lap, and squeeze your shoulder blades toward one another.\n" +
+                                "2. Focus on keeping your shoulders down, not hunched up toward your ears, and hold for 3 seconds.\n" +
+                                "3. Release and repeat 8 to 12 times.",
+                        "Shoulderblade squeeze"
                     ),
                     ProgramWorkoutItem(
-                        R.drawable.wall_pushups,
-                        "1. Stand about 3 feet away from a wall, facing the wall, with your feet shoulder-width apart.\n" +
-                                "2. Lean forward and place your hands flat on the wall, in line with your shoulders. Your body should be in plank position, with your spine straight, not sagging or arched.\n" +
-                                "3. Lower your body toward the wall and then push back.\n" +
-                                "4. Repeat 10 times.",
-                        "Placeholder"
+                        R.drawable.toe_taps,
+                        "1. Sitting in a chair and keeping your heels on the floor, lift your toes high enough that you can feel the muscles along your shin working. (This helps keep blood circulating in your legs and also strengthens the lower leg.)\n" +
+                                "2. Repeat 20 times.",
+                        "Toe taps"
                     )
                 )
             ),
             ProgramWorkout(
                 2, mutableListOf(
                     ProgramWorkoutItem(
-                        R.drawable.wall_pushups,
-                        "1. Stand about 3 feet away from a wall, facing the wall, with your feet shoulder-width apart.\n" +
-                                "2. Lean forward and place your hands flat on the wall, in line with your shoulders. Your body should be in plank position, with your spine straight, not sagging or arched.\n" +
-                                "3. Lower your body toward the wall and then push back.\n" +
-                                "4. Repeat 10 times.",
-                        "Wall pushups"
+                        R.drawable.heel_raises,
+                        "1. Sitting in a chair, keep your toes and the balls of your feet on the floor and lift your heels.\n" +
+                                "2. Repeat 20 times.",
+                        "Heel raises"
                     ),
                     ProgramWorkoutItem(
-                        R.drawable.bodyandsoul,
-                        "blabla",
-                        "Pushups"
+                        R.drawable.knee_lifts,
+                        "1. Seated in a chair, with your arms resting but not pressing on the armrests, contract your right quadriceps muscles and lift your leg. Your knee and the back of your thigh should be 2 or 3 inches off the seat.\n" +
+                                "2. Pause for 3 seconds and slowly lower your leg.\n" +
+                                "3. Complete 8 to 12 repetitions and then repeat with the opposite leg.",
+                        "Knee Lifts"
                     ),
                     ProgramWorkoutItem(
-                        R.drawable.fitandfab,
-                        "blabla",
-                        "Pushups"
-                    ),
-                    ProgramWorkoutItem(
-                        R.drawable.bodyandsoul,
-                        "blabla",
-                        "Pushups"
+                        R.drawable.ankle_rotations,
+                        "1. Seated in a chair, lift your right foot off the floor and slowly rotate your foot 5 times to the right and then 5 times to the left.\n" +
+                                "2. Repeat with the left foot.",
+                        "Ankle rotations"
                     )
                 )
             ),
             ProgramWorkout(
                 3, mutableListOf(
                     ProgramWorkoutItem(
-                        R.drawable.wall_pushups,
-                        "1. Stand about 3 feet away from a wall, facing the wall, with your feet shoulder-width apart.\n" +
-                                "2. Lean forward and place your hands flat on the wall, in line with your shoulders. Your body should be in plank position, with your spine straight, not sagging or arched.\n" +
-                                "3. Lower your body toward the wall and then push back.\n" +
-                                "4. Repeat 10 times.",
-                        "Wall pushups"
+                        R.drawable.neck_stretch,
+                        "1. Stand with your feet flat on the floor, shoulder-width apart. Keep your hands relaxed at your sides.\n" +
+                                "2. Don’t tip your head forward or backward as you turn your head slowly to the right. Stop when you feel a slight stretch. Hold for 10 to 30 seconds.\n" +
+                                "3. Now turn to the left. Hold for 10 to 30 seconds.\n" +
+                                "4. Repeat 3 to 5 times.",
+                        "Neck stretch"
                     ),
                     ProgramWorkoutItem(
-                        R.drawable.bodyandsoul,
-                        "blabla",
-                        "Pushups"
+                        R.drawable.back_stretch,
+                        "1. Sit in a firm chair. Place your feet flat on the floor, shoulder-width apart.\n" +
+                                "2. Hold your arms up and out in front at shoulder height, with your palms facing outward and the backs of your hands pressed together. Relax your shoulders so they’re not scrunched up near your ears.\n" +
+                                "3. Reach your fingertips out until you feel a stretch. Your back will move away from the back of the chair.\n" +
+                                "4. Stop and hold for 10 to 30 seconds.\n" +
+                                "5. Repeat 3 to 5 times.",
+                        "Back stretch"
                     ),
                     ProgramWorkoutItem(
-                        R.drawable.fitandfab,
-                        "blabla",
-                        "Pushups"
+                        R.drawable.knee_lifts,
+                        "1. Seated in a chair, with your arms resting but not pressing on the armrests, contract your right quadriceps muscles and lift your leg. Your knee and the back of your thigh should be 2 or 3 inches off the seat.\n" +
+                                "2. Pause for 3 seconds and slowly lower your leg.\n" +
+                                "3. Complete 8 to 12 repetitions and then repeat with the opposite leg.",
+                        "Knee Lifts"
                     ),
                     ProgramWorkoutItem(
-                        R.drawable.bodyandsoul,
-                        "blabla",
-                        "Pushups"
+                        R.drawable.ankle_rotations,
+                        "1. Seated in a chair, lift your right foot off the floor and slowly rotate your foot 5 times to the right and then 5 times to the left.\n" +
+                                "2. Repeat with the left foot.",
+                        "Ankle rotations"
                     )
                 )
             ),
             ProgramWorkout(
                 4, mutableListOf(
                     ProgramWorkoutItem(
-                        R.drawable.wall_pushups,
-                        "1. Stand about 3 feet away from a wall, facing the wall, with your feet shoulder-width apart.\n" +
-                                "2. Lean forward and place your hands flat on the wall, in line with your shoulders. Your body should be in plank position, with your spine straight, not sagging or arched.\n" +
-                                "3. Lower your body toward the wall and then push back.\n" +
-                                "4. Repeat 10 times.",
-                        "Wall pushups"
+                        R.drawable.knee_lifts,
+                        "1. Seated in a chair, with your arms resting but not pressing on the armrests, contract your right quadriceps muscles and lift your leg. Your knee and the back of your thigh should be 2 or 3 inches off the seat.\n" +
+                                "2. Pause for 3 seconds and slowly lower your leg.\n" +
+                                "3. Complete 8 to 12 repetitions and then repeat with the opposite leg.",
+                        "Knee Lifts"
                     ),
                     ProgramWorkoutItem(
-                        R.drawable.bodyandsoul,
-                        "blabla",
-                        "Pushups"
+                        R.drawable.ankle_rotations,
+                        "1. Seated in a chair, lift your right foot off the floor and slowly rotate your foot 5 times to the right and then 5 times to the left.\n" +
+                                "2. Repeat with the left foot.",
+                        "Ankle rotations"
                     ),
                     ProgramWorkoutItem(
-                        R.drawable.fitandfab,
-                        "blabla",
-                        "Pushups"
+                        R.drawable.neck_stretch,
+                        "1. Stand with your feet flat on the floor, shoulder-width apart. Keep your hands relaxed at your sides.\n" +
+                                "2. Don’t tip your head forward or backward as you turn your head slowly to the right. Stop when you feel a slight stretch. Hold for 10 to 30 seconds.\n" +
+                                "3. Now turn to the left. Hold for 10 to 30 seconds.\n" +
+                                "4. Repeat 3 to 5 times.",
+                        "Neck stretch"
                     ),
                     ProgramWorkoutItem(
-                        R.drawable.bodyandsoul,
-                        "blabla",
-                        "Pushups"
+                        R.drawable.back_stretch,
+                        "1. Sit in a firm chair. Place your feet flat on the floor, shoulder-width apart.\n" +
+                                "2. Hold your arms up and out in front at shoulder height, with your palms facing outward and the backs of your hands pressed together. Relax your shoulders so they’re not scrunched up near your ears.\n" +
+                                "3. Reach your fingertips out until you feel a stretch. Your back will move away from the back of the chair.\n" +
+                                "4. Stop and hold for 10 to 30 seconds.\n" +
+                                "5. Repeat 3 to 5 times.",
+                        "Back stretch"
                     )
                 )
             )
